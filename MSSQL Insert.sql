c8a6362bccaab0ba9c5530178c0ff88636e44138
dekanfrus <john.alves01@gmail.com>
2015-11-12 12:19:01 -0600

    Updates

---
+++ MSSQL Insert.sql
@@ -0,0 +1,29 @@
+INSERT INTO [User] (UserUsername, UserPassword, UserEmail, UserFName, UserLName, UserCountry, UserStreet, UserCity, UserZip, UserPhone) 
+VALUES ('GHopkins', 'Hopk!ns73', 'ghopkins@hopkins.net', 'Gene', 'Hopkins', 'USA', '112 Street', 'Corpus Christi', '78412', '3611234567');
+
+	INSERT INTO [User] (UserUsername, UserPassword, UserEmail, UserFName, UserLName, UserCountry, UserStreet, UserCity, UserZip, UserPhone) 
+	VALUES ('Joker', 'Batman*Sucks1', 'jnicholson@joker.com', 'Jack', 'Nicholson', 'USA', '2933 Joker Drive', 'Atlanta', '78417', '3616465185');
+	
+	INSERT INTO [User] (UserUsername, UserPassword, UserEmail, UserFName, UserLName, UserCountry, UserStreet, UserCity, UserZip, UserPhone) 
+	VALUES ('Shifu', 'KungFu@IsB3st', 'shifu@kungfu.com', 'Dustin', 'Hoffman', 'Mexico', '4460 Shifu Street', 'Tijuana', '78402', '3617283165');
+	
+	INSERT INTO [User] (UserUsername, UserPassword, UserEmail, UserFName, UserLName, UserCountry, UserStreet, UserCity, UserZip, UserPhone) 
+	VALUES ('FGump', 'Bubb4.Gump*', 'chocolates@bgshrimp.com', 'Tom', 'Hanks', 'United Kingdom','3173 Lincoln Court', 'London', '78465', '3611303328');
+	
+	INSERT INTO [User] (UserUsername, UserPassword, UserEmail, UserFName, UserLName, UserCountry, UserStreet, UserCity, UserZip, UserPhone) 
+	VALUES ('Hannibal', 'P3opleRTasty)', 'Chef@hannibal.com', 'Anthony', 'Hopkins', 'New Zealand', '2982 Hannibal Street', 'Wellington', '78410', '3612695776');
+	
+	INSERT INTO [User] (UserUsername, UserPassword, UserEmail, UserFName, UserLName, UserCountry, UserStreet, UserCity, UserZip, UserPhone) 
+	VALUES ('MalcomX', 'Bl4ck.Power!', 'malcom@bpower.com', 'Denzel', 'Washington', 'USA', '6729 Unstoppable Ave', 'Seattle', '78416', '3618093010');
+	
+	INSERT INTO [User] (UserUsername, UserPassword, UserEmail, UserFName, UserLName, UserCountry, UserStreet, UserCity, UserZip, UserPhone) 
+	VALUES ('God', 'Sup4h!S3cur3*', 'god@almighty.com', 'Morgan', 'Freeman', 'USA', '9483 Sundance Drive', 'New York City' , '78414', '3613180737');
+	
+	INSERT INTO [User] (UserUsername, UserPassword, UserEmail, UserFName, UserLName, UserCountry, UserStreet, UserCity, UserZip, UserPhone) 
+	VALUES ('Flubber', 'C4pta!nH00k', 'president@tomdobbs.com', 'Robin', 'Williams', 'USA', '4551 Flubber Drive', 'San Diego', '78418', '3619062552');
+	
+	INSERT INTO [User] (UserUsername, UserPassword, UserEmail, UserFName, UserLName, UserCountry, UserStreet, UserCity, UserZip, UserPhone) 
+	VALUES ('Katniss', 'Arch3ry*is*B3st?', 'mockingjay@disctrict13.com', 'Jennifer', 'Lawrence', 'Panem', '6367 District Twelve Ave', 'D12', '78416', '3618726651');
+	
+	INSERT INTO [User] (UserUsername, UserPassword, UserEmail, UserFName, UserLName, UserCountry, UserStreet, UserCity, UserZip, UserPhone) 
+	VALUES ('PBunny', 'L!kesT0Pl4y&', 'easterbunny@playboy.com', 'Kate', 'Upton', 'USA', '6813 Playboy Court', 'Los Angeles', '76135', '3618162885');
---
+++ SQL Update User.sql
@@ -0,0 +1,1 @@
+UPDATE [User] SET UserEmail='president@tomdobbs.com' WHERE UserID=8;
