2a66b2009324ebe8bd23b47d41c7420b52675c2b
dekanfrus <john.alves01@gmail.com>
2015-11-27 18:57:12 -0600

    More work on the shopping cart

--- TripleJAuto/TripleJAuto/App_Code/Cart.cs
+++ TripleJAuto/TripleJAuto/App_Code/Cart.cs
@@ -7,5 +7,27 @@ namespace TripleJAuto.App_Code
 {
     public class Cart
     {
+        public Cart() { }
+        public Cart(Product product, int quantity)
+        {
+            this.Product = product;
+            this.Quantity = quantity;
+        }
+
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public void AddQuantity(int quantity)
+        {
+            this.Quantity += quantity;
+        }
+
+        public string Display()
+        {
+            string display = string.Format("{0} ({1} at {2} each)",
+                Product.Name, Quantity.ToString(),
+                Product.UnitPrice.ToString("c"));
+            return display;
+        }
     }
 }
